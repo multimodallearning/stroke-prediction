262946b12233733c41325914d233d2bacb6e1808
Christian Lucas <lucas@imi.uni-luebeck.de>
2019-01-17 17:22:09 +0100

    added lesion position module (L1 loss... check for classification approach!) added 2nd order derivative computation to be used on grid_outputs for loss

--- GRUnet_2.py
+++ GRUnet_2.py
@@ -8,6 +8,49 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def def_vec2vec(n_dim, final_activation=None, init_fn=lambda x: x):
+    assert len(n_dim) > 1
+
+    result = []
+    for i in range(1, len(n_dim) - 1):
+        result += [nn.Linear(n_dim[i - 1], n_dim[i]), nn.ReLU(True)]
+    result += [nn.Linear(n_dim[len(n_dim) - 2], n_dim[len(n_dim) - 1])]
+
+    if final_activation:
+        if final_activation.lower() == 'relu':
+            result.append(nn.ReLU())
+        elif final_activation.lower() == 'sigmoid':
+            result.append(nn.Sigmoid())
+        elif final_activation.lower() == 'tanh':
+            result.append(nn.Tanh())
+        else:
+            raise AssertionError('Unknown final activation function')
+
+    return nn.Sequential(*init_fn(result))
+
+
+def def_img2vec(n_dim, ksize=(1, 3, 3), psize=(0, 1, 1), dsize=(1, 2, 2)):
+    assert len(n_dim) == 5
+    return nn.Sequential(
+        nn.InstanceNorm3d(n_dim[0]),  # 128x128x28
+        nn.Conv3d(n_dim[0], n_dim[1], kernel_size=ksize, padding=psize),  # 128x128x28
+        nn.ReLU(),
+        nn.MaxPool3d(4, 4),  # 32x32x7
+        nn.InstanceNorm3d(n_dim[1]),
+        nn.Conv3d(n_dim[1], n_dim[2], kernel_size=ksize, padding=psize),  # 32x32x7
+        nn.ReLU(),
+        nn.MaxPool3d(dsize,dsize),  # 16x16x7
+        nn.InstanceNorm3d(n_dim[2]),
+        nn.Conv3d(n_dim[2], n_dim[3], kernel_size=ksize),  # 14x14x7
+        nn.ReLU(),
+        nn.MaxPool3d(dsize, dsize),  # 7x7x7
+        nn.InstanceNorm3d(n_dim[3]),
+        nn.Conv3d(n_dim[3], n_dim[4], kernel_size=1),  # 7x7x7
+        nn.ReLU(),
+        nn.MaxPool3d(7)  # 1x1x1
+    )
+
+
 class GRUnetBlock(nn.Module):
     def __init__(self, input_size, hidden_size, kernel_size, output_size=None):
         super().__init__()
@@ -166,63 +209,20 @@ class AffineModule(nn.Module):
 
         return _init
 
-    def def_img2vec(self, n_dim, ksize=(1, 3, 3), psize=(0, 1, 1), dsize=(1, 2, 2)):
-        assert len(n_dim) == 5
-        return nn.Sequential(
-            nn.InstanceNorm3d(n_dim[0]),  # 128x128x28
-            nn.Conv3d(n_dim[0], n_dim[1], kernel_size=ksize, padding=psize),  # 128x128x28
-            nn.ReLU(),
-            nn.MaxPool3d(4, 4),  # 32x32x7
-            nn.InstanceNorm3d(n_dim[1]),
-            nn.Conv3d(n_dim[1], n_dim[2], kernel_size=ksize, padding=psize),  # 32x32x7
-            nn.ReLU(),
-            nn.MaxPool3d(dsize,dsize),  # 16x16x7
-            nn.InstanceNorm3d(n_dim[2]),
-            nn.Conv3d(n_dim[2], n_dim[3], kernel_size=ksize),  # 14x14x7
-            nn.ReLU(),
-            nn.MaxPool3d(dsize, dsize),  # 7x7x7
-            nn.InstanceNorm3d(n_dim[3]),
-            nn.Conv3d(n_dim[3], n_dim[4], kernel_size=1),  # 7x7x7
-            nn.ReLU(),
-            nn.MaxPool3d(7)  # 1x1x1
-        )
-
-    def def_vec2vec(self, n_dim, final_activation=None, init_fn=lambda x: x):
-        assert len(n_dim) > 1
-
-        result = []
-        for i in range(1, len(n_dim)-1):
-            result += [nn.Linear(n_dim[i-1], n_dim[i]), nn.ReLU(True)]
-        result += [nn.Linear(n_dim[len(n_dim)-2], n_dim[len(n_dim)-1])]
-
-        if final_activation:
-            if final_activation.lower() == 'relu':
-                result.append(nn.ReLU())
-            elif final_activation.lower() == 'sigmoid':
-                result.append(nn.Sigmoid())
-            elif final_activation.lower() == 'tanh':
-                result.append(nn.Tanh())
-            else:
-                raise AssertionError('Unknown final activation function')
-
-        return nn.Sequential(*init_fn(result))
-
     def __init__(self, dim_img2vec, dim_vec2vec, dim_clinical, kernel_size, seq_len):
         super().__init__()
 
         dim_in_img = dim_img2vec[0]
         dim_hidden = dim_img2vec[-1] + dim_clinical
         assert dim_vec2vec[0] == dim_hidden
-        assert len(dim_img2vec) == 5
-        assert len(dim_vec2vec) == 4
         assert dim_vec2vec[-1] == 24  # core and penumbra affine parameters
 
         self.len = seq_len
 
         self.affine1 = GRUnetBlock(dim_in_img, dim_in_img, kernel_size)
-        self.affine2 = self.def_img2vec(n_dim=dim_img2vec)
+        self.affine2 = def_img2vec(n_dim=dim_img2vec)
         self.affine3 = nn.GRUCell(dim_hidden, dim_hidden, bias=True)
-        self.affine4 = self.def_vec2vec(n_dim=dim_vec2vec, init_fn=self.init_theta(-1))
+        self.affine4 = def_vec2vec(n_dim=dim_vec2vec, init_fn=self.init_theta(-1))
         self.affine5 = nn.GRUCell(24, 24, bias=True)
 
     def forward(self, input_img, clinical, out_size, hidden_affine1, hidden_affine3, hidden_affine5):
@@ -236,6 +236,31 @@ class AffineModule(nn.Module):
         grid_penu = nn.functional.affine_grid(hidden_affine5[:, 12:].view(-1, 3, 4), out_size)
 
         return torch.cat((grid_core, grid_penu), dim=4), hidden_affine1, hidden_affine3, hidden_affine5
+
+
+class LesionPosModule(nn.Module):
+    def __init__(self, dim_img2vec, dim_vec2vec, dim_clinical, kernel_size, seq_len):
+        super().__init__()
+
+        dim_in_img = dim_img2vec[0]
+        dim_hidden = dim_img2vec[-1] + dim_clinical
+        assert dim_vec2vec[0] == dim_hidden
+
+        self.len = seq_len
+
+        self.affine1 = GRUnetBlock(dim_in_img, dim_in_img, kernel_size)
+        self.affine2 = def_img2vec(n_dim=dim_img2vec)
+        self.affine3 = nn.GRUCell(dim_hidden, dim_hidden, bias=True)
+        self.affine4 = def_vec2vec(n_dim=dim_vec2vec, final_activation='sigmoid')
+
+    def forward(self, input_img, clinical, hidden_affine1, hidden_affine3, hidden_affine5):
+
+        hidden_affine1, affine1 = self.affine1(input_img, hidden_affine1)
+        affine2 = self.affine2(affine1)
+        hidden_affine3 = self.affine3(torch.cat((affine2, clinical), dim=1).squeeze(), hidden_affine3)
+        affine4 = self.affine4(hidden_affine3)
+
+        return affine4, hidden_affine1, hidden_affine3, hidden_affine5
 
 
 class UnidirectionalNet(nn.Module):
@@ -260,10 +285,12 @@ class UnidirectionalNet(nn.Module):
         return marker1, marker2, marker3, marker
     '''
 
-    def __init__(self, n_ch_grunet, dim_img2vec, dim_vec2vec, dim_clinical, dim_feat_rnn, kernel_size, seq_len):
+    def __init__(self, n_ch_grunet, dim_img2vec_affine, dim_vec2vec_affine, dim_img2vec_time, dim_vec2vec_time,
+                 dim_clinical, dim_feat_rnn, kernel_size, seq_len, batchsize=4):
         super().__init__()
 
         self.len = seq_len
+        self.batchsize = batchsize
 
         #
         # Separate (hidden) features for core / penumbra
@@ -272,13 +299,18 @@ class UnidirectionalNet(nn.Module):
 
         #
         # Affine
-        assert dim_img2vec[0] == 2 * dim_feat_rnn + 2  # + core/penumbra
-        assert dim_img2vec[-1] + dim_clinical == dim_vec2vec[0]
-        self.affine = AffineModule(dim_img2vec, dim_vec2vec, dim_clinical, kernel_size, seq_len)
+        assert dim_img2vec_affine[0] == 2 * dim_feat_rnn + 2  # + core/penumbra
+        assert dim_img2vec_affine[-1] + dim_clinical == dim_vec2vec_affine[0]
+        self.affine = AffineModule(dim_img2vec_affine, dim_vec2vec_affine, dim_clinical, kernel_size, seq_len)
 
         #
         # Non-lin.
         self.grunet = GRUnet(hidden_sizes=n_ch_grunet, kernel_sizes=[kernel_size] * 5, down_scaling=2)
+
+        #
+        # Time position
+        assert dim_img2vec_time[-1] + dim_clinical == dim_vec2vec_time[0]
+        self.time = LesionPosModule(dim_img2vec_time, dim_vec2vec_time, dim_clinical, kernel_size, seq_len)
 
         '''
         #
@@ -302,13 +334,17 @@ class UnidirectionalNet(nn.Module):
 
     def forward(self, core, penu, core_rep, penu_rep, clinical):
         offset = []
+        pr_time = []
 
         hidden_core = None
         hidden_penu = None
         hidden_grunet = None
         hidden_affine1 = None
-        hidden_affine3 = torch.zeros((4, self.affine.affine3.hidden_size)).cuda()
-        hidden_affine5 = torch.zeros((4, 24)).cuda()
+        hidden_affine3 = torch.zeros((self.batchsize, self.affine.affine3.hidden_size)).cuda()
+        hidden_affine5 = torch.zeros((self.batchsize, 24)).cuda()
+        hidden_time1 = None
+        hidden_time3 = torch.zeros((self.batchsize, self.time.affine3.hidden_size)).cuda()
+        hidden_time5 = torch.zeros((self.batchsize, 1)).cuda()
 
         for i in range(self.len):
             hidden_core, core_rep = self.core_rep(core_rep, hidden_core)
@@ -320,8 +356,12 @@ class UnidirectionalNet(nn.Module):
                                                                                        hidden_affine5)
 
             input_grunet = torch.cat((input_img, affine_grids.permute(0,4,1,2,3)), dim=1)
-            hidden_grunet, last_output = self.grunet(input_grunet, hidden_grunet)
-            offset.append(last_output)
+            hidden_grunet, nonlin_grids = self.grunet(input_grunet, hidden_grunet)
+            offset.append(nonlin_grids)
+
+            pr_t, hidden_time1, hidden_time3, hidden_time5 = self.time(torch.cat((input_img, nonlin_grids.permute(0,4,1,2,3)), dim=1),
+                                                                       clinical, hidden_time1, hidden_time3, hidden_time5)
+            pr_time.append(pr_t)
 
             '''
             # marker
@@ -330,7 +370,7 @@ class UnidirectionalNet(nn.Module):
             marker.append(scalar)
             '''
 
-        return offset
+        return offset, pr_time
 
 
 class BidirectionalSequence(nn.Module):
@@ -344,8 +384,8 @@ class BidirectionalSequence(nn.Module):
             nn.ReLU()
         )
 
-    def __init__(self, n_ch_feature_single, n_ch_affine_img2vec, n_ch_affine_vec2vec, n_ch_grunet, n_ch_clinical,
-                 kernel_size, seq_len, batch_size=4, out_size=6):
+    def __init__(self, n_ch_feature_single, n_ch_affine_img2vec, n_ch_affine_vec2vec, dim_img2vec_time,
+                 dim_vec2vec_time, n_ch_grunet, n_ch_clinical, kernel_size, seq_len, batch_size=4, out_size=6):
         super().__init__()
         self.len = seq_len
         assert seq_len > 0
@@ -357,10 +397,10 @@ class BidirectionalSequence(nn.Module):
 
         ##################################
         # Part 2: Bidirectional Recurrence
-        self.rnn1 = UnidirectionalNet(n_ch_grunet, n_ch_affine_img2vec, n_ch_affine_vec2vec, n_ch_clinical,
-                                      n_ch_feature_single, kernel_size, seq_len)
-        self.rnn2 = UnidirectionalNet(n_ch_grunet, n_ch_affine_img2vec, n_ch_affine_vec2vec, n_ch_clinical,
-                                      n_ch_feature_single, kernel_size, seq_len)
+        self.rnn1 = UnidirectionalNet(n_ch_grunet, n_ch_affine_img2vec, n_ch_affine_vec2vec, dim_img2vec_time,
+                                      dim_vec2vec_time, n_ch_clinical, n_ch_feature_single, kernel_size, seq_len)
+        self.rnn2 = UnidirectionalNet(n_ch_grunet, n_ch_affine_img2vec, n_ch_affine_vec2vec, dim_img2vec_time,
+                                      dim_vec2vec_time, n_ch_clinical, n_ch_feature_single, kernel_size, seq_len)
 
         ################################################
         # Part 3: Combine predictions of both directions
@@ -379,24 +419,29 @@ class BidirectionalSequence(nn.Module):
 
         ##################################
         # Part 2: Bidirectional Recurrence
-        offset1 = self.rnn1(core, penu, core_rep, penu_rep, clinical)
-        offset2 = self.rnn2(core, penu, core_rep, penu_rep, clinical)
+        offset1, time1 = self.rnn1(core, penu, core_rep, penu_rep, clinical)
+        offset2, time2 = self.rnn2(core, penu, core_rep, penu_rep, clinical)
 
         ################################################
         # Part 3: Combine predictions of both directions
-        offsets = [factor[:, i]*offset1[i] + (1-factor[:, i])*offset2[self.len - i - 1] for i in range(self.len)]
+        offsets = [factor[:, i] * offset1[i] + (1 - factor[:, i]) * offset2[self.len - i - 1] for i in range(self.len)]
+        t_fucts = [factor[:, i].squeeze() * time1[i].squeeze() + (1 - factor[:, i]).squeeze() * time2[self.len - i - 1].squeeze() for i in range(self.len)]
         del offset1
         del offset2
+        del time1
+        del time2
 
         output_by_core = []
         output_by_penu = []
-        output_factors = []
+        #output_factors = []
 
         for i in range(self.len):
+            '''
             fc = factor[:, i]
             zero = torch.zeros(fc.size(), requires_grad=False).cuda()
             ones = torch.ones(fc.size(), requires_grad=False).cuda()
             output_factors.append(torch.where(fc < 0.5, zero, ones))
+            '''
 
             offsets[i] = self.soften(offsets[i])
             pred_by_core = nn.functional.grid_sample(core, self.grid_identity + offsets[i][:, :, :, :, :3])
@@ -404,4 +449,4 @@ class BidirectionalSequence(nn.Module):
             output_by_core.append(pred_by_core)
             output_by_penu.append(pred_by_penu)
 
-        return torch.cat(output_by_core, dim=1), torch.cat(output_by_penu, dim=1)  #, torch.cat(marker, dim=1),  torch.cat(output_factors, dim=1)
+        return torch.cat(output_by_core, dim=1), torch.cat(output_by_penu, dim=1), torch.stack(t_fucts, dim=1)  #, torch.cat(marker, dim=1),  torch.cat(output_factors, dim=1)
--- train_grunet.py
+++ train_grunet.py
@@ -10,31 +10,54 @@ class Criterion(nn.Module):
         super(Criterion, self).__init__()
         self.dc = metrics.BatchDiceLoss(weights)  # weighted inversely by each volume proportion
         self.dc_mid = metrics.BatchDiceLoss([1.0])  # weighted inversely by each volume proportion
-        self.ce = nn.CrossEntropyLoss()
+        self.l1 = nn.L1Loss()
+        #self.ce = nn.CrossEntropyLoss()
 
-    def forward(self, pred, target, output, out_c, out_p):
+    def compute_2nd_order_derivative(self, x):
+        a = torch.Tensor([[[1, 0, -1], [2, 0, -2], [1, 0, -1]],
+                          [[1, 0, -1], [2, 0, -2], [1, 0, -1]],
+                          [[1, 0, -1], [2, 0, -2], [1, 0, -1]]])
+        a = a.view((1, 1, 3, 3, 3))
+        G_x = nn.functional.conv3d(x, a)
+
+        b = torch.Tensor([[[1, 2, 1], [0, 0, 0], [-1, -2, -1]],
+                          [[1, 2, 1], [0, 0, 0], [-1, -2, -1]],
+                          [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]])
+        b = b.view((1, 1, 3, 3, 3))
+        G_y = nn.functional.conv3d(x, b)
+
+        b = torch.Tensor([[[1, 2, 1], [1, 2, 1], [1, 2, 1]],
+                          [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                          [[-1, -2, -1], [-1, -2, -1], [-1, -2, -1]]])
+        b = b.view((1, 1, 3, 3, 3))
+        G_z = nn.functional.conv3d(x, b)
+
+        return torch.sqrt(torch.pow(G_x, 2) + torch.pow(G_y, 2) + torch.pow(G_z, 2))
+
+    def forward(self, pred, target, output, out_c, out_p, pr_t_lesion, t_lesion):
         loss = 0.5 * self.dc(pred, target)
-        loss += 0.25 * self.dc_mid(out_c, out_p)
+        loss += 0.2 * self.dc_mid(out_c, out_p)
 
         for i in range(output.size()[1]-1):
             diff = output[:, i+1] - output[:, i]
-            loss += 0.025 * torch.mean(torch.abs(diff) - diff)  # monotone
+            loss += 0.02 * torch.mean(torch.abs(diff) - diff)  # monotone
 
+        loss += 0.2 * self.l1(pr_t_lesion, t_lesion)
         #loss += 0.2 * self.ce(torch.stack((1-marker_pred.flatten(),
         #                                   marker_pred.flatten()), dim=1), marker_target.flatten().long())
 
         return loss
 
 
-def get_title(prefix, idx, batch, seq_len):
+def get_title(prefix, row, idx, batch, seq_len, time):
     suffix = ''
     if idx == int(batch[data.KEY_GLOBAL][row, 0, :, :, :]):
-        suffix += '/C'
+        suffix += ' C'
     elif idx == int(batch[data.KEY_GLOBAL][row, 1, :, :, :]):
-        suffix += '/L'
+        suffix += ' L'
     elif idx == seq_len-1:
-        suffix += '/P'
-    return '{}{}'.format(str(idx), suffix)
+        suffix += ' P'
+    return '{}({:1.1f}){}'.format(str(idx), float(time[row, idx]), suffix)
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -47,9 +70,11 @@ batchsize = 4
 sequence_length = 10
 num_clinical_input = 2
 n_ch_feature_single = 11
-n_ch_affine_img2vec = [24, 28, 32, 36, 40]
-n_ch_affine_vec2vec = [42, 36, 30, 24]
-n_ch_grunet_input_additional = 8  # 1 core + 1 penumbra + 3 affine core + 3 affine penumbra
+n_ch_affine_img2vec = [24, 28, 32, 36, 40]  # first layer dim: 2 * n_ch_feature_single + 2 core/penu segmentation; list of length = 5
+n_ch_affine_vec2vec = [42, 36, 30, 24]  # first layer dim: last layer dim of img2vec + 2 clinical scalars; list of arbitrary length > 1
+n_ch_additional_grid_input = 8  # 1 core + 1 penumbra + 3 affine core + 3 affine penumbra
+n_ch_time_img2vec = [30, 32, 34, 36, 38]  #
+n_ch_time_vec2vec = [40, 30, 15, 1]  #
 n_ch_grunet = [30, 40, 50, 40, 30]
 zslice = zsize // 2
 pad = (20, 20, 20)
@@ -71,9 +96,11 @@ ds_train, ds_valid = data.get_toy_seq_shape_training_data(train_trafo, valid_tra
                                                           batchsize=batchsize, normalize=sequence_length, growth='fast',
                                                           zsize=zsize)
 
-assert n_ch_grunet[0] == 2 * n_ch_feature_single + n_ch_grunet_input_additional
-bi_net = BidirectionalSequence(n_ch_feature_single, n_ch_affine_img2vec, n_ch_affine_vec2vec, n_ch_grunet,
-                               num_clinical_input, kernel_size=convgru_kernel, seq_len=sequence_length).to(device)
+assert n_ch_grunet[0] == 2 * n_ch_feature_single + n_ch_additional_grid_input
+assert n_ch_time_img2vec[0] == 2 * n_ch_feature_single + n_ch_additional_grid_input
+bi_net = BidirectionalSequence(n_ch_feature_single, n_ch_affine_img2vec, n_ch_affine_vec2vec, n_ch_time_img2vec,
+                               n_ch_time_vec2vec, n_ch_grunet, num_clinical_input, kernel_size=convgru_kernel,
+                               seq_len=sequence_length).to(device)
 
 params = [p for p in bi_net.parameters() if p.requires_grad]
 print('# optimizing params', sum([p.nelement() * p.requires_grad for p in params]),
@@ -101,23 +128,22 @@ for epoch in range(0, 200):
         for batch in ds_train:
             gt = batch[data.KEY_LABELS].to(device)
 
-            factor = torch.tensor([[1] * sequence_length] * batchsize, dtype=torch.float).cuda()
+            factor = torch.tensor([[0] * sequence_length] * batchsize, dtype=torch.float).cuda()
             marker_target = torch.tensor(factor)
             for b in range(batchsize):
-                t_lesion =int(batch[data.KEY_GLOBAL][b, 1, :, :, :])
+                t_lesion = int(batch[data.KEY_GLOBAL][b, 1, :, :, :])
                 t_core = int(batch[data.KEY_GLOBAL][b, 0, :, :, :])
                 length = sequence_length - t_core
                 t_half = t_core + length // 2
                 factor[b, :t_core] = 1
                 factor[b, t_core:t_half] = torch.tensor([1 - i / length for i in range(length//2)], dtype=torch.float).cuda()
                 factor[b, t_half:] = torch.tensor([(length//2) / length - i / length for i in range(length - length//2)], dtype=torch.float).cuda()
-                marker_target[b, :t_lesion] = 0
-                marker_target[b, t_lesion:] = 1
+                marker_target[b, t_lesion] = 1
 
-            out_c, out_p = bi_net(gt[:, int(batch[data.KEY_GLOBAL][b, 0, :, :, :]), :, :, :].unsqueeze(1),
-                                  gt[:, -1, :, :, :].unsqueeze(1),
-                                  batch[data.KEY_GLOBAL].to(device),
-                                  factor)
+            out_c, out_p, pr_t_lesion = bi_net(gt[:, int(batch[data.KEY_GLOBAL][b, 0, :, :, :]), :, :, :].unsqueeze(1),
+                                               gt[:, -1, :, :, :].unsqueeze(1),
+                                               batch[data.KEY_GLOBAL].to(device),
+                                               factor)
             #output = factor.unsqueeze(2).unsqueeze(3).unsqueeze(4) * out_c + (1-factor).unsqueeze(2).unsqueeze(3).unsqueeze(4) * out_p
             output = 0.5 * out_c + 0.5 * out_p
 
@@ -133,7 +159,9 @@ for epoch in range(0, 200):
                              gt[mask].view(batchsize, 3, zsize, 128, 128),
                              output,
                              out_c[mask_t_lesion].view(batchsize, 1, zsize, 128, 128),
-                             out_p[mask_t_lesion].view(batchsize, 1, zsize, 128, 128))
+                             out_p[mask_t_lesion].view(batchsize, 1, zsize, 128, 128),
+                             pr_t_lesion,
+                             marker_target)
             loss_mean += loss.item()
 
             optimizer.zero_grad()
@@ -148,10 +176,10 @@ for epoch in range(0, 200):
             titles = []
             for i in range(sequence_length):
                 axarr[row, i].imshow(gt.cpu().detach().numpy()[row, i, zslice, :, :], vmin=0, vmax=1, cmap='gray')
-                titles.append(get_title('GT', i, batch, sequence_length))
+                titles.append(get_title('GT', row, i, batch, sequence_length, marker_target))
             for i in range(sequence_length):
                 axarr[row, i + sequence_length].imshow(output.cpu().detach().numpy()[row, i, zslice, :, :], vmin=0, vmax=1, cmap='gray')
-                titles.append(get_title('Pr', i, batch, sequence_length))
+                titles.append(get_title('Pr', row, i, batch, sequence_length, pr_t_lesion))
             for ax, title in zip(axarr[row], titles):
                 ax.set_title(title)
         del batch
@@ -172,23 +200,22 @@ for epoch in range(0, 200):
         for batch in ds_valid:
             gt = batch[data.KEY_LABELS].to(device)
 
-            factor = torch.tensor([[1] * sequence_length] * batchsize, dtype=torch.float).cuda()
+            factor = torch.tensor([[0] * sequence_length] * batchsize, dtype=torch.float).cuda()
             marker_target = torch.tensor(factor)
             for b in range(batchsize):
-                t_lesion =int(batch[data.KEY_GLOBAL][b, 1, :, :, :])
+                t_lesion = int(batch[data.KEY_GLOBAL][b, 1, :, :, :])
                 t_core = int(batch[data.KEY_GLOBAL][b, 0, :, :, :])
                 length = sequence_length - t_core
                 t_half = t_core + length // 2
                 factor[b, :t_core] = 1
                 factor[b, t_core:t_half] = torch.tensor([1 - i / length for i in range(length//2)], dtype=torch.float).cuda()
                 factor[b, t_half:] = torch.tensor([(length//2) / length - i / length for i in range(length - length//2)], dtype=torch.float).cuda()
-                marker_target[b, :t_lesion] = 0
-                marker_target[b, t_lesion:] = 1
+                marker_target[b, t_lesion] = 1
 
-            out_c, out_p = bi_net(gt[:, int(batch[data.KEY_GLOBAL][b, 0, :, :, :]), :, :, :].unsqueeze(1),
-                                          gt[:, -1, :, :, :].unsqueeze(1),
-                                          batch[data.KEY_GLOBAL].to(device),
-                                          factor)
+            out_c, out_p, pr_t_lesion = bi_net(gt[:, int(batch[data.KEY_GLOBAL][b, 0, :, :, :]), :, :, :].unsqueeze(1),
+                                               gt[:, -1, :, :, :].unsqueeze(1),
+                                               batch[data.KEY_GLOBAL].to(device),
+                                               factor)
             #output = factor.unsqueeze(2).unsqueeze(3).unsqueeze(4) * out_c + (1-factor).unsqueeze(2).unsqueeze(3).unsqueeze(4) * out_p
             output = 0.5 * out_c + 0.5 * out_p
 
@@ -204,7 +231,9 @@ for epoch in range(0, 200):
                              gt[mask].view(batchsize, 3, zsize, 128, 128),
                              output,
                              out_c[mask_t_lesion].view(batchsize, 1, zsize, 128, 128),
-                             out_p[mask_t_lesion].view(batchsize, 1, zsize, 128, 128))
+                             out_p[mask_t_lesion].view(batchsize, 1, zsize, 128, 128),
+                             pr_t_lesion,
+                             marker_target)
             loss_mean += loss.item()
 
             inc += 1
@@ -215,10 +244,10 @@ for epoch in range(0, 200):
             titles = []
             for i in range(sequence_length):
                 axarr[row + n_visual_samples, i].imshow(gt.cpu().detach().numpy()[row, i, zslice, :, :], vmin=0, vmax=1, cmap='gray')
-                titles.append(get_title('GT', i, batch, sequence_length))
+                titles.append(get_title('GT', row, i, batch, sequence_length, marker_target))
             for i in range(sequence_length):
                 axarr[row + n_visual_samples, i + sequence_length].imshow(output.cpu().detach().numpy()[row, i, zslice, :, :], vmin=0, vmax=1, cmap='gray')
-                titles.append(get_title('Pr', i, batch, sequence_length))
+                titles.append(get_title('Pr', row, i, batch, sequence_length, pr_t_lesion))
             for ax, title in zip(axarr[row + n_visual_samples], titles):
                 ax.set_title(title)
         del batch
