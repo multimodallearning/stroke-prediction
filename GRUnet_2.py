Increase network capability, Fix loss computation

GRUnet_2.py
@@ -854,9 +854,9 @@ class BiNet(nn.Module):
         self.visual_grid = self.visualise_grid(batch_size)
 
         # Feature part
-        channels_feature = [2, 16, 24]
-        channels_img2vec = [24, 32, 48, 64]
-        channels_vec2vec = [64 + 2, 32]
+        channels_feature = [2, 16, 32]
+        channels_img2vec = [32, 48, 64, 72]
+        channels_vec2vec = [72 + 2, 61]
 
         self.feature_core = self._feature(channels_feature)
         self.img2vec_core = self._img2vec(channels_img2vec)
@@ -867,11 +867,13 @@ class BiNet(nn.Module):
         self.vec2vec_penu = self._vec2vec(channels_vec2vec)
 
         # Recurrent part
-        self.vec2gru_core = nn.GRUCell(32 + 1, 22)
-        self.gru2gru_core = nn.GRUCell(22, 12)
+        self.vec2gru_core = nn.GRUCell(61 + 3, 64)
+        self.gru2gru_core = nn.GRUCell(64, 48)
+        self.gru2aff_core = nn.GRUCell(48, 12)
 
-        self.vec2gru_penu = nn.GRUCell(32 + 1, 22)
-        self.gru2gru_penu = nn.GRUCell(22, 12)
+        self.vec2gru_penu = nn.GRUCell(61 + 3, 64)
+        self.gru2gru_penu = nn.GRUCell(64, 48)
+        self.gru2aff_penu = nn.GRUCell(48, 12)
 
     def forward(self, core, penu, clinical):
         pred_core = []
@@ -880,9 +882,12 @@ class BiNet(nn.Module):
         grid_penu = []
 
         hidden_vec2gru_core = torch.zeros(self.grid_identity_core.size(0), self.vec2gru_core.hidden_size).cuda()
-        hidden_gru2gru_core = self._affine_identity().view(-1, 12).expand(self.grid_identity_core.size(0), 12).cuda()
+        hidden_gru2gru_core = torch.zeros(self.grid_identity_core.size(0), self.gru2gru_core.hidden_size).cuda()
+        hidden_gru2aff_core = self._affine_identity().view(-1, 12).expand(self.grid_identity_core.size(0), 12).cuda()
+
         hidden_vec2gru_penu = torch.zeros(self.grid_identity_penu.size(0), self.vec2gru_penu.hidden_size).cuda()
-        hidden_gru2gru_penu = self._affine_identity().view(-1, 12).expand(self.grid_identity_penu.size(0), 12).cuda()
+        hidden_gru2gru_penu = torch.zeros(self.grid_identity_penu.size(0), self.gru2gru_penu.hidden_size).cuda()
+        hidden_gru2aff_penu = self._affine_identity().view(-1, 12).expand(self.grid_identity_penu.size(0), 12).cuda()
 
         blob_core = self.feature_core(torch.cat((core, penu), dim=1))
         blob_core = self.img2vec_core(blob_core)
@@ -895,18 +900,20 @@ class BiNet(nn.Module):
         blob_penu = self.vec2vec_penu(blob_penu.squeeze())
 
         for i in range(self.len):
-            vec_core = torch.cat((blob_core, torch.ones(self.grid_identity_core.size(0), 1).cuda() * i), dim=1)
-            vec_penu = torch.cat((blob_penu, torch.ones(self.grid_identity_penu.size(0), 1).cuda() * i), dim=1)
+            vec_core = torch.cat((blob_core, clinical.squeeze(), torch.ones(self.grid_identity_core.size(0), 1).cuda() * i), dim=1)
+            vec_penu = torch.cat((blob_penu, clinical.squeeze(), torch.ones(self.grid_identity_penu.size(0), 1).cuda() * i), dim=1)
 
             hidden_vec2gru_core = self.vec2gru_core(vec_core, hidden_vec2gru_core)
             hidden_gru2gru_core = self.gru2gru_core(hidden_vec2gru_core, hidden_gru2gru_core)
-            offsets_core = affine_grid(hidden_gru2gru_core.view(-1, 3, 4), core.size())
+            hidden_gru2aff_core = self.gru2aff_core(hidden_gru2gru_core, hidden_gru2aff_core)
+            offsets_core = affine_grid(hidden_gru2aff_core.view(-1, 3, 4), core.size())
             pred_core.append(grid_sample(core, self.grid_identity_core + offsets_core))
             grid_core.append(grid_sample(self.visual_grid, self.grid_identity_core + offsets_core))
 
             hidden_vec2gru_penu = self.vec2gru_penu(vec_penu, hidden_vec2gru_penu)
             hidden_gru2gru_penu = self.gru2gru_penu(hidden_vec2gru_penu, hidden_gru2gru_penu)
-            offsets_penu = affine_grid(hidden_gru2gru_penu.view(-1, 3, 4), penu.size())
+            hidden_gru2aff_penu = self.gru2aff_penu(hidden_gru2gru_penu, hidden_gru2aff_penu)
+            offsets_penu = affine_grid(hidden_gru2aff_penu.view(-1, 3, 4), penu.size())
             pred_penu.append(grid_sample(penu, self.grid_identity_penu + offsets_penu))
             grid_penu.append(grid_sample(self.visual_grid, self.grid_identity_penu + offsets_penu))
 

train_grunet.py
@@ -113,17 +113,15 @@ class Criterion_BiNet(nn.Module):
                 pr_penu_c, pr_penu_p, gt_penu, output):
 
         loss = self.weights[0] * self.multi_scale_dc(pr_core_c, gt_core)
-        loss += self.weights[1] * self.multi_scale_dc(pr_lesion_c, gt_lesion)
-        loss += self.weights[2] * self.multi_scale_dc(pr_penu_c, gt_penu)
-        loss = self.weights[3] * self.multi_scale_dc(pr_core_c, gt_core)
-        loss += self.weights[4] * self.multi_scale_dc(pr_lesion_c, gt_lesion)
-        loss += self.weights[5] * self.multi_scale_dc(pr_penu_c, gt_penu)
+        loss += self.weights[1] * self.multi_scale_dc(pr_core_p, gt_core)
+        loss += self.weights[2] * self.multi_scale_dc(pr_lesion_c, gt_lesion)
+        loss += self.weights[3] * self.multi_scale_dc(pr_lesion_p, gt_lesion)
+        loss += self.weights[4] * self.multi_scale_dc(pr_penu_c, gt_penu)
+        loss += self.weights[5] * self.multi_scale_dc(pr_penu_p, gt_penu)
 
         for i in range(output.size()[1]-1):
             diff = output[:, i+1] - output[:, i]
             loss += self.weights[6] * torch.mean(torch.abs(diff) - diff)  # monotone
-
-        loss += nn.L1Loss()
 
         return loss
 
@@ -339,7 +337,7 @@ def process_batch_BiNet(batch, batchsize, bi_net, criterion, sequence_length, se
                                                                                                     sequence_thresholds,
                                                                                                     t_core, idx_core)
 
-    prs = pred_core * 0.5 + 0.5 * pred_penu
+    prs = pred_core * factor.unsqueeze(2).unsqueeze(3).unsqueeze(4) + (1-factor).unsqueeze(2).unsqueeze(3).unsqueeze(4) * pred_penu
 
     loss = criterion(pr_core_c, pr_core_p,
                      gt[:, 0, :, :, :].unsqueeze(1),
@@ -587,10 +585,9 @@ def main_BiNet(arg_path, arg_batchsize, arg_clinical, arg_commonfeature, arg_add
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     batchsize = arg_batchsize
-    sequence_thresholds = [0.0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.2, 4.5, 4.8, 5.1,
-                           5.4, 5.7, 6.0, 6.4, 7.1, 8.5, 10.0]
+    sequence_thresholds = [.0, .2, .4, .6, .8, 1., 1.2, 1.4, 1.6, 1.8, 2., 2.2, 2.4, 2.6, 2.8, 3., 3.2, 3.4, 3.6, 3.8, 4., 4.2, 4.4, 4.6, 4.8, 5., 5.3, 5.6, 5.9, 6.2, 6.5, 7., 8., 10.]
     sequence_length = len(sequence_thresholds)
-    n_visual_samples = min(4, batchsize)
+    n_visual_samples = 2
 
     modalities = ['_CBV_reg1_downsampled',
                   '_TTD_reg1_downsampled']